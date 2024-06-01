1ed376b8eb0668e5bd2f7c8bca9d54ca5678deb3
smppatel99999 <smppatel99999@gmail.com>
2024-06-01 03:15:16 +0000

    feat: add homepage, login, signup

---
+++ pages/auth/signin.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="text-white">
+        Signin
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-3xl text-white">
+        Hello
+    </div>
+  </template>
+  
