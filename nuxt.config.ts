3431f3577c2cd52d0cb37b4b8c05147e754c4d29
smppatel99999 <smppatel99999@gmail.com>
2024-06-01 04:13:27 +0000

    chore: add runtime config for Appwrite URL & Project ID

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  runtimeConfig: {
+    public: {
+      backendAPIBase: process.env.APPWRITE_CLOUD_BASE_URL,
+      backendProjectId: process.env.APPWRITE_PROJECT_ID,
+    }
+  }
 })
